fde3c296d376c5d6d293f8965b1223ea0673f25a
gitdenlev <levchenkoden02@gmail.com>
2023-04-17 11:51:58 +0200

    Update code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
 
+const stopBtn = document.querySelector('button[data-stop]');
+
+const body = document.querySelector('body');
+
+let colorInterval = null;
+
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  colorInterval = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(colorInterval);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
